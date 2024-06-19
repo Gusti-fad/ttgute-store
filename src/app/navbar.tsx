37b48ec357c3d4f208d97de3e4fd7fae1acf0582
Gusti <gfadhillah06@gmail.com>
2024-06-19 15:57:10 +0700

    first commit

---
+++ src/app/navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import Image from "next/image"
+export default function Navbar() {
+    return (
+        <nav className="navbar bg-white w-full fixed z-10 flex justify-between py-5 px-48">
+            <Link href="/">
+                <p className="poppins text-2xl font-semibold text-[#130f40]">TTGUTE STORE</p>
+            </Link>
+            <ul className="flex text-[#666] poppins text-[17px] items-center">
+                    {/* <Link className="mr-5" href="#home"> */}
+                        <li className="mr-5"><a href="#home">Home</a></li>
+                    {/* </Link>
+                    <Link className="mr-5" href="#about"> */}
+                        <li className="mr-5"><a href="#about">About</a></li>
+                    {/* </Link>
+                    <Link className="mr-5" href="#bestseller1"> */}
+                        <li className="mr-5"><a href="#bestseller1">Best Seller</a></li>
+                    {/* </Link>
+                    <Link className="mr-5" href="#bestseller2"> */}
+                        <li className="mr-5"><a href="#bestseller2">Product</a></li>
+                    {/* </Link>
+                    <Link className="mr-5" href="/contact"> */}
+                        <li>Contact</li>
+                    {/* </Link> */}
+            </ul>
+            <div className="flex">
+            <Image className="mr-2" src="/home/cart.svg" alt="" width={35} height={35} />
+            <Image src="/home/profile.svg" alt="" width={30} height={30} />
+            </div>
+        </nav>);
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,63 @@
 import Image from "next/image";
+import Bg from '/public/keyboard/home6.jpg';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="poppins">
+    <section id="home" className="py-16 px-56 leading-normal min-h-[100vh] justify-end flex items-center relative" style={{backgroundImage: `url(${Bg.src})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div className="">
+        <div className="">
+          <h3 className="poppins font-bold text-white text-6xl leading-normal"><span className="text-[#e67e22]">Hi </span>Welcome To <br /> TTGUTE STORE</h3>
+          <Link href="/product">
+            <button className="px-7 py-2 border-2 rounded-r-full rounded-bl-full text-white">Shop Now</button>
+          </Link>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          <Image src="/keyboard/bottom_wave.png" className="w-full h-32 absolute bottom-0 left-0" quality={100} alt="" width={1955} height={230}/>
+    </section>
+    <section id="about" className="px-40 py-12 flex items-center justify-between">
+      <div className="w-[40%]">
+        <Image src='/keyboard/switch.jpg' className="w-full" width={1024} height={1024} alt="" />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-[60%]">
+        <h3 className="font-semibold text-4xl">Best <span className="text-[#e67e22]">Keyboard</span> For You</h3>
+        <p className="py-3 text-[#666]">TTGUTE STORE Menyediakan Berbagai Macam Keyboard Yang Dapat Menambah Performa Bekerja Atau Bermain Game Anda.</p>
+        <Link href="/product">
+          <button className="px-7 py-2 border-2 border-[#130f40] rounded-r-full rounded-bl-full text-[#130f40]">Read More</button>
+        </Link>
       </div>
-    </main>
+    </section>
+    <section id="bestseller1" className="px-40 py-12 flex items-center justify-between">
+      <div className="w-[48%]">
+        <Image src="/keyboard/mojo68.jpg" width={700} height={700} alt=""/>
+      </div>
+      <div className="w-1/2 text-center">
+        <h3 className="font-semibold text-4xl"><span className="text-[#e67e22]">Melgeek Mojo68</span> Plastic</h3>
+        <p className="py-3 text-[#666]">Melgeek Mojo68 Plastic Adalah Kerboard Yang Memiliki Desain Yang Futuristic Yang Dapat Membuat Meja Kerja Anda Terlihat Keren.</p>
+        <div className="text-4xl text-[#130f40] py-1">
+          $199.99
+        </div>
+        <Link href="/product">
+          <button className="mt-3 px-7 py-2 border-2 border-[#130f40] rounded-r-full rounded-bl-full text-[#130f40]">Buy Now</button>
+        </Link>
+      </div>
+    </section>
+    <section id="bestseller2" className="px-40 py-12 flex items-center justify-between">
+      <div className="w-1/2 text-center">
+        <h3 className="font-semibold text-4xl"><span className="text-[#e67e22]">Glorious </span> GMMK Pro</h3>
+        <p className="py-3 text-[#666]">Glorious GMMK Pro Adalah Keyboard Barebone Yang Memiliki Build Quality Yang Sangat Bagus Dan Body Yang Sangat Solid.</p>
+        <div className="text-4xl text-[#130f40] py-1">
+          $170.00
+        </div>
+        <Link href="/product">
+          <button className="mt-3 px-7 py-2 border-2 border-[#130f40] rounded-r-full rounded-bl-full text-[#130f40]">Buy Now</button>
+        </Link>
+      </div>
+      <div className="w-[48%]">
+        <Image src="/keyboard/glorius.jpg" width={1200} height={960} alt=""/>
+      </div>
+    </section>
+    </div>
   );
 }
