4b0d72615c9210dd72d13ed652d196143de59369
Gusti <gfadhillah06@gmail.com>
2024-06-20 15:35:52 +0700

    fix product name

--- src/app/product.tsx
+++ src/app/product.tsx
@@ -17,7 +17,7 @@ export default function Product() {
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
@@ -28,7 +28,7 @@ export default function Product() {
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
@@ -39,7 +39,7 @@ export default function Product() {
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
@@ -50,62 +50,62 @@ export default function Product() {
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
                 <Image src="/keyboard/barang9.jpg" className="rounded-xl w-full group-hover:scale-110 duration-200" style={{objectFit: 'cover'}} width={540} height={540} alt=""/>
             </div>
             <div className="px-4 py-5 grid grid-cols-7">
-                <h3 className="text-xl font-medium col-span-4">Numb Gray</h3>
+                <h3 className="text-xl font-medium col-span-4">Monster Blue</h3>
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
                 <Image src="/keyboard/barang5.jpg" className="rounded-xl w-full group-hover:scale-110 duration-200" style={{objectFit: 'cover'}} width={540} height={540} alt=""/>
             </div>
             <div className="px-4 py-5 grid grid-cols-7">
-                <h3 className="text-xl font-medium col-span-4">Monster Blue</h3>
+                <h3 className="text-xl font-medium col-span-4">Numb White</h3>
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
                 <Image src="/keyboard/barang6.jpg" className="rounded-xl w-full group-hover:scale-110 duration-200" style={{objectFit: 'cover'}} width={540} height={540} alt=""/>
             </div>
             <div className="px-4 py-5 grid grid-cols-7">
-                <h3 className="text-xl font-medium col-span-4">Numb White</h3>
+                <h3 className="text-xl font-medium col-span-4">Numb Purple</h3>
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
                 <Image src="/keyboard/barang7.jpg" className="rounded-xl w-full group-hover:scale-110 duration-200" style={{objectFit: 'cover'}} width={540} height={540} alt=""/>
             </div>
             <div className="px-4 py-5 grid grid-cols-7">
-                <h3 className="text-xl font-medium col-span-4">Numb Purple</h3>
+                <h3 className="text-xl font-medium col-span-4">Numb Orange</h3>
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
             <div id="product-1" className="shadow-xl rounded-xl p-3 w-96 group cursor-pointer">
             <div className="w-full rounded-xl overflow-hidden">
                 <Image src="/keyboard/barang8.jpg" className="rounded-xl w-full group-hover:scale-110 duration-200" style={{objectFit: 'cover'}} width={540} height={540} alt=""/>
             </div>
             <div className="px-4 py-5 grid grid-cols-7">
-                <h3 className="text-xl font-medium col-span-4">Numb Orange</h3>
+                <h3 className="text-xl font-medium col-span-4">Numb Gray</h3>
                 <p className="text-xl col-span-3 text-end">$199.00</p>
                 <p className="col-span-7 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, alias!</p>
             </div>
-            <button className="w-full py-1 rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
+            <button className="w-full py-[5px] rounded-lg bg-gray-200 mb-3 hover:bg-gray-300 duration-200">Add to Cart</button>
             </div>
         </div>
     </section>
