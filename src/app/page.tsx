59af24bee083252cd795ad0979af4224f7186806
Gusti <gfadhillah06@gmail.com>
2024-06-20 15:09:55 +0700

    fix bug home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <div className="poppins">
     <section id="home" className="md:py-16 md:px-56 leading-normal min-h-[100vh] justify-center md:justify-end flex items-center relative" style={{backgroundImage: `url(${Bg.src})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
         <div className="">
-          <h3 className="poppins font-bold text-white text-3xl text-center md:text-start md:text-6xl leading-normal"><span className="text-[#e67e22]">Hi </span>Welcome To <br /> TTGUTE STORE</h3>
-          <Link href="/product" className="flex justify-center">
+          <h3 className="poppins font-semibold text-white text-3xl text-center md:text-start md:text-6xl leading-normal md:leading-normal"><span className="text-[#e67e22]">Hi </span>Welcome To <br /> TTGUTE STORE</h3>
+          <Link href="/product" className="flex justify-center md:justify-start">
             <button className="md:px-7 md:py-2 px-5 py-1 border-2 rounded-r-full rounded-bl-full text-white mt-2">Shop Now</button>
           </Link>
         </div>
