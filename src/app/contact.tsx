e20e5e88d998174a77d8c4673a459533306a69e5
Gusti <gfadhillah06@gmail.com>
2024-06-27 09:58:56 +0700

    add hover submit button

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
                     <input type="email" name="email" id="email" placeholder="Your Email" className="w-full py-4 text-xl my-3 focus:outline-none border-b border-slate-800"/>
                     <input type="text" name="number" id="number" placeholder="Your Number" className="w-full py-4 text-xl my-3 focus:outline-none border-b border-slate-800"/>
                     <textarea name="message" id="message" placeholder="Your Message" className="w-full h-[150px] py-4 text-xl my-3 focus:outline-none border-b border-slate-800"></textarea>
-                    <input type="submit" name="submit" id="submit" value={"Send Message"} className="w-full h-full mt-5 py-2 px-7 border-2 border-[#130f40] text-[17px] rounded-r-full rounded-bl-full leading-normal" />
+                    <input type="submit" name="submit" id="submit" value={"Send Message"} className="w-full h-full mt-5 py-2 px-7 border-2 border-blue-950 text-blue-950 hover:bg-blue-950 duration-200 hover:text-white text-[17px] rounded-r-full rounded-bl-full leading-normal" />
                 </div>
             </div>
         </section>
