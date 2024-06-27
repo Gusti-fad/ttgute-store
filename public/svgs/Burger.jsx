8155c16eabcd01bd5802efc2cb15b9a0773257a8
Gusti <gfadhillah06@gmail.com>
2024-06-27 11:43:34 +0700

    add burger menu

---
+++ public/svgs/Burger.jsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+const SvgComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
+
+    <g id="_icons">
+      <path d="M5 7h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1zM5 13h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1zM5 19h14c.6 0 1-.4 1-1s-.4-1-1-1H5c-.6 0-1 .4-1 1s.4 1 1 1z" />
+    </g>
+  </svg>
+)
+export default SvgComponent
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,34 +1,41 @@
+"use client"
 import Link from "next/link"
 import Image from "next/image"
 import Cart from "../../public/svgs/Cart"
 import Profile from "../../public/svgs/Profile"
+import Burger from "../../public/svgs/Burger"
+
+
 export default function Navbar() {
     return (
-        <nav className="navbar bg-white w-full fixed z-10 hidden md:flex justify-between py-5 px-48">
-            <Link href="/">
-                <p className="poppins text-2xl font-semibold text-[#130f40]">TTGUTE STORE</p>
-            </Link>
-            <ul className="flex text-[#666] poppins text-[17px] items-center">
-                    {/* <Link className="mr-5" href="#home"> */}
-                        <li className="mr-5"><a href="#home">Home</a></li>
-                    {/* </Link>
-                    <Link className="mr-5" href="#about"> */}
-                        <li className="mr-5"><a href="#about">About</a></li>
-                    {/* </Link>
-                    <Link className="mr-5" href="#bestseller1"> */}
-                        <li className="mr-5"><a href="#bestseller1">Best Seller</a></li>
-                    {/* </Link>
-                    <Link className="mr-5" href="#bestseller2"> */}
-                        <li className="mr-5"><a href="#product">Product</a></li>
-                    {/* </Link>
-                    <Link className="mr-5" href="/contact"> */}
-                        <li><a href="#contact">Contact</a></li>
-                    {/* </Link> */}
-            </ul>
-            <div className="flex">
-                <Cart className="h-9 w-9 "/>
-                <Profile  className="h-9 w-9" />
-            </div>
-        </nav>);
+        <div className="w-full navbar bg-white flex justify-center fixed z-10 py-5 drop-shadow-lg" data-aos="fade-down" data-aos-duration="500" data-aos-anchor="#about">
+            <nav className="max-w-[1200px] w-full flex justify-between px-3">
+                <Link href="/">
+                    <p className="poppins text-2xl font-semibold text-[#130f40] flex items-center h-full">TTGUTE STORE</p>
+                </Link>
+                <ul className="hidden md:flex duration-300 text-[#666] poppins text-[17px] items-center">
+                        {/* <Link className="mr-5" href="#home"> */}
+                            <li className="mr-5"><a href="#home">Home</a></li>
+                        {/* </Link>
+                        <Link className="mr-5" href="#about"> */}
+                            <li className="mr-5"><a href="#about">About</a></li>
+                        {/* </Link>
+                        <Link className="mr-5" href="#bestseller1"> */}
+                            <li className="mr-5"><a href="#bestseller1">Best Seller</a></li>
+                        {/* </Link>
+                        <Link className="mr-5" href="#bestseller2"> */}
+                            <li className="mr-5"><a href="#product">Product</a></li>
+                        {/* </Link>
+                        <Link className="mr-5" href="/contact"> */}
+                            <li><a href="#contact">Contact</a></li>
+                        {/* </Link> */}
+                </ul>
+                <div className="flex">
+                    <Burger className="h-10 w-10 mr-2 flex items-center justify-center hover:text-orange-400 duration-300 md:hidden"/>
+                    <Cart className="h-10 w-10 mr-2 hover:text-orange-400 duration-300"/>
+                    <Profile  className="h-9 w-9 hover:text-orange-400 duration-300" />
+                </div>
+            </nav>
+        </div>);
 }
 
