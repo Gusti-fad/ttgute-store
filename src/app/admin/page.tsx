b8f1bb2f99b33c991afe9a556fe0184360e3a5d0
Gusti <gfadhillah06@gmail.com>
2024-07-18 09:40:28 +0700

    admin page (prototype)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,41 +1,45 @@
 "use client"
+import Image from "next/image"
+import { useState } from "react"
 
-import { PrismaClient } from "@prisma/client";
-
-const submitProduct = async (event:any) => {
-    event.preventDefault();
-    
-    const data = {
-        item_name : event.currentTarget.product_name.value,
-        price : event.currentTarget.price.value
-    }
-    const response = await fetch('/api/addProduct', {
-        method: 'POST',
-        body: JSON.stringify(data),
-    });
-    // const collection = await response.json();
-};
 export default function Admin() {
+    const [qty, setQty] = useState(0)
+    let awal = 0
+    const handleChange = (e: any) => {
+        setQty(e.target.value)
+        console.log(qty)
+    }
+    const maxLengthCheck = (object : any) => {
+        if (object.target.value.length > object.target.maxLength) {
+            object.target.value = object.target.value.slice(0, object.target.maxLength)
+        }
+        }
     return (
-        <section id="admin" className="pt-40 pb-20 w-full min-h-screen bg-slate-200">
-            <div className="w-[80%] bg-white mx-auto rounded-xl p-20">
-                <h1 className="font-bold text-4xl mb-10">Add New Product</h1>
-                <div>
-                <form onSubmit={(e) => submitProduct(e)}  className="flex justify-between flex-wrap">
-                    <div className="w-[49%]">
-                        <label htmlFor="product_name" className="text-lg w-full">Pruduct Name <b className="text-red-500">*</b></label>
-                        <input placeholder=" E.g. Black Orange" type="text" name="product_name" id="product_name" required className="border w-full border-slate-800 outline-none p-3 rounded-md mt-2"/>
+        <section className="pt-32 px-10 bg-gray-100 pb-40">
+            <h1 className="font-poppins font-semibold text-2xl text-slate-800 text-center">Admin Page</h1>
+            <div className="flex justify-center gap-5 mt-10">
+                <div className="bg-gray-50 rounded-xl shadow-lg p-14 w-2/5">
+                    <h2 className="font-poppins font-semibold text-xl text-slate-800">Stock Management </h2>
+                    <div className="mt-10 grid gap-y-5">
+                        <div className="w-full rounded-lg flex justify-between items-center">
+                            <div className="flex items-center">
+                                <Image src="/keyboard/barang1.jpg" className="mr-3" width={50} height={50} alt=""/>
+                                <p className="font-poppins min-w-40 mr-3">Black Orange</p>
+                            </div>
+                            <div className="flex">
+                                <button className="border border-gray-200 h-10 w-10 border-r-0" onClick={() => qty > 0 ? setQty(qty - 1) : setQty(0)}>-</button>
+                                <div className="border-y border-gray-200 h-10 w-10 flex mx-auto">
+                                <input type="number" value={qty} onChange={e => setQty(parseInt(e.target.value))} onInput={maxLengthCheck} maxLength={4} className=" h-full w-full outline-none text-center"/>
+                                </div>
+                                <button className="border border-gray-200 h-10 w-10 border-l-0" onClick={() => qty >= 9999 ? setQty(qty) : setQty(qty + 1)}>+</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-[49%]">
-                        <label htmlFor="product_name" className="text-lg w-full">Price <b className="text-red-500">*</b></label>
-                        <input placeholder=" E.g. 100" type="number" min={0} name="price" id="price" required className="border w-full border-slate-800 outline-none p-3 rounded-md mt-2"/>
-                    </div>
-                    <div className="w-full flex justify-end pt-10">
-                        <button className="py-2 px-7 bg-blue-500 text-white hover:bg-blue-700 rounded-lg duration-300">Submit</button>
-                    </div>
-                </form>
+                </div>
+                <div className="bg-gray-50 rounded-xl shadow-lg py-20 px-10 ">
+                    <h2 className="font-poppins font-semibold text-xl text-slate-800">Product Management </h2>
                 </div>
             </div>
         </section>
-    );
+    )
 }
